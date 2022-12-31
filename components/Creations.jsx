ced2ce5fa30b34ef23521c27cf3c3fa396d175b7
presh-031 <akingbulugbe.dev@gmail.com>
2022-12-31 14:56:25 +0100

    finished marking up creations component

---
+++ components/Creations.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Creations = () => (
+  <div>
+    <div>
+      <p>Our creations</p>
+      <div>
+        <p>SEE ALL</p>
+      </div>
+
+      {/* The grid container is below */}
+      <div></div>
+    </div>
+  </div>
+);
+
+export default Creations;
