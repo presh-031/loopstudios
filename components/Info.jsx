1f5f9493857a061f24cc2c621446d8381816b720
presh-031 <akingbulugbe.dev@gmail.com>
2022-12-31 21:02:22 +0100

    finished styling info component

--- components/Info.jsx
+++ components/Info.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Info = () => {
   return (
-    <div className="text-center px-6 py-20">
-      <div className="bg-info-img-mobile w-full h-[14rem] mb-8  bg-no-repeat bg-cover"></div>
+    <div className="px-6 py-20 text-center font-josefin-sans">
+      <div className="mb-8 h-[14rem] w-full bg-info-img-mobile  bg-cover bg-no-repeat"></div>
       <div>
-        <p>THE LEADER IN INTERACTIVE VR</p>
-        <p>
+        <p className="mb-4 text-3xl">THE LEADER IN INTERACTIVE VR</p>
+        <p className="mx-auto w-[90%] font-semibold">
           Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best
           companies around the globe. Our award-winning creations have transformed businesses through digital
           experiences that bind to their brand.
