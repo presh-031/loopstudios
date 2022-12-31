d66049e08597bb28ee8c84c94a94a27d25a4c4f2
presh-031 <akingbulugbe.dev@gmail.com>
2022-12-31 14:54:29 +0100

    finished marking up info component

---
+++ components/Info.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Info = () => {
+  return (
+    <div>
+      <div>{/* The image will be a bg-img */}</div>
+      <div>
+        <p>THE LEADER IN INTERACTIVE VR</p>
+        <p>
+          Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best
+          companies around the globe. Our award-winning creations have transformed businesses through digital
+          experiences that bind to their brand.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import HeadComponent from "../components/HeadComponent";
 import Hero from "../components/Hero";
+import Info from "../components/Info";
 
 export default function Home() {
   return (
     <div className="bg-gray-900">
       <HeadComponent page="HomePage" />
       <Hero />
+      <Info />
     </div>
   );
 }
